ba90c5d06e46006da791eed9df811563a5266c82
Li <15311759611@163.com>
2016-08-09 11:01:15 +0800

    no message

---
+++ system/config/database.inc.php
@@ -0,0 +1,5 @@
+<?php
+return array(
+'default' => array (
+'hostname' => '127.0.0.1','database' => 'u282605184_lx','username' => 'u282605184_lx','password' => '1012~!@','tablepre' => 'go_','charset' => 'utf8','type' => 'db_mysql','debug' => false,'pconnect' => 0,'autoconnect' => 0),
+);
